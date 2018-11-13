f2f05842fdf7e23944e4ae3f319948d7b11c9fad
rainrocka <qqqq2900@126.com>
2018-11-13 18:23:06 +0800

    系统修复

--- web/res/js/jquery-rockupload.js
+++ web/res/js/jquery-rockupload.js
@@ -137,7 +137,7 @@
 				for(var i=0;i<a.length;i++)if(a[i].id)ids+=','+a[i].id+'';
 				if(ids!='')ids=ids.substr(1);
 				try{if(form(this.fileidinput))form(this.fileidinput).value=ids;}catch(e){};
-				if(oi>0)this.allsuccess(this.fileallarr, ids); //必须全部才触发
+				this.allsuccess(this.fileallarr, ids); //必须全部才触发
 				return false;
 			}
 			this.nowoi = oi;
